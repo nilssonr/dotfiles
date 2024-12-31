59df73d23e1e385f42d348fa395cbe096386ba97
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2024-12-30 17:20:05 -0700

    added roslyn config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,11 @@ vim.wo.relativenumber = true
 vim.o.cmdheight = 0
 
 vim.o.clipboard = "unnamed,unnamedplus"
+
+-- Auto-refresh code lens
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+	pattern = "*.cs", -- Adjust the file type if needed
+	callback = function()
+		vim.lsp.codelens.refresh()
+	end,
+})
--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -1,5 +1,21 @@
 return {
 	"seblj/roslyn.nvim",
 	ft = "cs",
-	opts = {},
+	opts = {
+		config = {
+			settings = {
+				["csharp|code_lens"] = {
+					dotnet_enable_references_code_lens = true,
+				},
+				["csharp|symbol_search"] = {
+					dotnet_search_reference_assemblies = true,
+				},
+				["csharp|completion"] = {
+					dotnet_show_completion_items_from_unimported_namespaces = true,
+					dotnet_show_name_completion_suggestions = true,
+					dotnet_provide_regex_completions = true,
+				},
+			},
+		},
+	},
 }
