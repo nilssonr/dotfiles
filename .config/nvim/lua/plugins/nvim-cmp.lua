806fb2d9e4fab0cc36757e35a8e915c151d4038b
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2024-12-31 14:39:47 -0700

    opacity & borders on autocomplete

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -48,6 +48,10 @@ return { -- Autocompletion
 			-- end,
 			-- },
 			completion = { completeopt = "menu,menuone,noinsert" },
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
 
 			-- For an understanding of why these mappings were
 			-- chosen, you will need to read `:help ins-completion`
