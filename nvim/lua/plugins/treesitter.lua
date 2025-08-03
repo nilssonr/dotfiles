83bd31db18029b7164eed859c4ce984dc240e20f
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-08-03 17:40:18 -0600

    feat: added LSPs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   opts = {
-    ensure_installed = { "lua", "go", "json", "javascript", "typescript", "c_sharp", "norg", "norg_meta" },
+    ensure_installed = { "lua", "go", "json", "javascript", "typescript", "c_sharp", "norg", "norg_meta", "html", "css" },
     highlight = { enable = true, additional_vim_regex_highlighting = false },
     indent = { enable = true },
   },
