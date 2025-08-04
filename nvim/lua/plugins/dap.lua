87e66438dcc8364b1ced1617d78b650a9f78cfcd
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-08-04 13:06:30 -0600

    feat: added dap and debuggers

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require("dap")
+      require("dapui").setup()
+      require("dap.ext.vscode").load_launchjs()
+
+      -- Auto open/close UI
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap" },
+  }
+}
