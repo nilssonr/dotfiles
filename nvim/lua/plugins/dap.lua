2104dc5a0cbc3231f5f2c56d6431aa46629821bb
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-08-04 15:23:00 -0600

    fix: updated dap layout

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -6,7 +6,30 @@ return {
   },
   config = function()
     require("dap")
-    require("dapui").setup()
+    require("dapui").setup({
+      layouts = {
+        {
+          -- left sidebar (default type)
+          elements = {
+            { id = "scopes",      size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks",      size = 0.25 },
+            { id = "watches",     size = 0.25 },
+          },
+          size = 80, -- 👈 width in columns (default is 40)
+          position = "left",
+        },
+        {
+          -- bottom console + repl
+          elements = {
+            { id = "repl",    size = 0.5 },
+            { id = "console", size = 0.5 },
+          },
+          size = 10, -- height in lines
+          position = "bottom",
+        },
+      },
+    })
     require("dap.ext.vscode").load_launchjs()
 
     -- Auto open/close UI
