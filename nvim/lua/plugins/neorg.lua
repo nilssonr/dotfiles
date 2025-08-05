e6b89a7b756c2b5f5eca33c8e585018637b7ab4d
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-08-05 12:04:10 -0600

    fix: temp switchover to fixed neorg

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,5 +1,7 @@
 return {
-  "nvim-neorg/neorg",
+  -- "nvim-neorg/neorg",
+  "muwizayeon/neorg",
+  branch = "fix-journal-toc-load-v9.3.0",
   lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
   config = {
