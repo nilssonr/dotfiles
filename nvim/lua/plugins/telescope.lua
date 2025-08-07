2defd6af9dcdbdca9f2a66a47b3401e346c945cd
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-08-06 19:54:23 -0600

    feat: exclude lazy lock from telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,30 +13,12 @@ return {
         sorting_strategy = "ascending",
         file_ignore_patterns = {
           "node_modules/",
-          "package%-lock%.json"
+          "package%-lock%.json",
+          "lazy%-lock%.json"
         }
       },
       extensions = {
         file_browser = {
-          mappings = {
-            ["i"] = {
-              ["<C-c>"] = fb_actions.create,
-              ["<S-CR>"] = fb_actions.create_from_prompt,
-              ["<A-r>"] = fb_actions.rename,
-              ["<A-m>"] = fb_actions.move,
-              ["<A-y>"] = fb_actions.copy,
-              ["<A-d>"] = fb_actions.remove,
-              ["<C-o>"] = fb_actions.open,
-              ["<C-g>"] = fb_actions.goto_parent_dir,
-              ["<C-e>"] = fb_actions.goto_home_dir,
-              ["<C-w>"] = fb_actions.goto_cwd,
-              ["<C-t>"] = fb_actions.change_cwd,
-              ["<C-f>"] = fb_actions.toggle_browser,
-              ["<C-h>"] = fb_actions.toggle_hidden,
-              ["<C-s>"] = fb_actions.toggle_all,
-              ["<bs>"] = fb_actions.backspace,
-            }
-          }
         }
       }
     })
