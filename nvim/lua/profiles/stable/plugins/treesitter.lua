fd4afa2ff47f64ea774167febde2209b15551035
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2025-12-30 14:11:08 -0700

    chore: downgrade tree-sitter, wtf are they doing

--- nvim/lua/profiles/stable/plugins/treesitter.lua
+++ nvim/lua/profiles/stable/plugins/treesitter.lua
@@ -2,25 +2,25 @@
 -- Tree-sitter
 -- ===============================================================
 return {
-    "nvim-treesitter/nvim-treesitter", -- tree-sitter integration
-    lazy = false, -- load immediately
-    build = ":TSUpdate", -- update parsers on install
-    config = function()
-        -- New rewrite API: setup + install
-        require("nvim-treesitter").setup({}) -- initialize tree-sitter
-
-        -- Install a sensible baseline set (add/remove from this table)
-        require("nvim-treesitter").install({
-            "lua", -- Lua
-            "typescript", -- TypeScript
-            "tsx", -- TSX
-            "javascript", -- JavaScript
-            "json", -- JSON
-            "yaml", -- YAML
-            "toml", -- TOML
-            "go", -- Go
-            "rust", -- Rust
-            "c_sharp", -- C#
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "typescript",
+        "tsx",
+        "javascript",
+        "json",
+        "yaml",
+        "toml",
+        "go",
+        "rust",
+        "c_sharp",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
